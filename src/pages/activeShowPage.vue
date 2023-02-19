1a6c0dd990a5dfb30b4027c5b07d77a2d9309cf2
moathabdulrazak <moathabdulrazak12@gmail.com>
2023-02-19 01:34:45 -0700

    show funcitonality

--- src/pages/activeShowPage.vue
+++ src/pages/activeShowPage.vue
@@ -1,54 +1,106 @@
 <template>
-  <div>
-    <h1>{{ show?.name }}</h1>
-    <p>{{ show?.overview }}</p>
+  <div class="active-show-page">
+    <h1 class="show-title">{{ show?.name }}</h1>
+    <p class="show-overview">{{ show?.overview }}</p>
 
-    <h2>Seasons</h2>
-    <ul>
-      <li v-for="season in seasons" :key="season.id">
-        <h3>Season {{ season.season_number }}</h3>
-        <ul>
-          <li v-for="episode in season.episodes" :key="episode.id">
-            <router-link :to="{ name: 'episode', params: { id: show.id, season: season.season_number, episode: episode.episode_number }}">
-              {{ episode.name }}
-            </router-link>
-          </li>
-        </ul>
-      </li>
-    </ul>
+    <div class="seasons-dropdown">
+      <label for="season-select">Seasons:</label>
+      <select id="season-select" v-model="selectedSeason">
+        <option v-for="season in seasons" :key="season.id" :value="season.season_number">Season {{ season.season_number }}</option>
+      </select>
+    </div>
+
+    <div class="episodes-list">
+      <ul v-if="selectedSeason">
+        <li v-for="episode in getSelectedSeasonEpisodes()" :key="episode.id">
+          <router-link :to="{ name: 'episode', params: { id: show.id, season: selectedSeason, episode: episode.episode_number }}">
+            {{ episode.name }}
+          </router-link>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 const API_KEY = "88d2c735e36149b50c9d46f09826ec06";
-  export default {
-    name: "ActiveShowPage",
-    data() {
-      return {
-        show: null,
-        seasons: [],
-      };
-    },
-    async created() {
-      const showId = this.$route.params.id;
-      const url = `https://api.themoviedb.org/3/tv/${showId}?api_key=${API_KEY}&append_to_response=seasons`;
-      const response = await fetch(url);
-      const data = await response.json();
-      this.show = data;
+export default {
+  name: "ActiveShowPage",
+  data() {
+    return {
+      show: null,
+      seasons: [],
+      selectedSeason: null,
+    };
+  },
+  async created() {
+    const showId = this.$route.params.id;
+    const url = `https://api.themoviedb.org/3/tv/${showId}?api_key=${API_KEY}&append_to_response=seasons`;
+    const response = await fetch(url);
+    const data = await response.json();
+    this.show = data;
 
-      this.seasons = data.seasons;
-      this.seasons.forEach((season) => {
-        season.episodes = [];
-        const seasonUrl = `https://api.themoviedb.org/3/tv/${showId}/season/${season.season_number}?api_key=${API_KEY}`;
-        fetch(seasonUrl)
-          .then((response) => response.json())
-          .then((data) => {
-            season.episodes = data.episodes;
-          });
-      });
+    this.seasons = data.seasons;
+    this.selectedSeason = this.seasons[0].season_number;
+
+    this.seasons.forEach((season) => {
+      season.episodes = [];
+      const seasonUrl = `https://api.themoviedb.org/3/tv/${showId}/season/${season.season_number}?api_key=${API_KEY}`;
+      fetch(seasonUrl)
+        .then((response) => response.json())
+        .then((data) => {
+          season.episodes = data.episodes;
+        });
+    });
+  },
+  methods: {
+    getSelectedSeasonEpisodes() {
+      return this.seasons.find((season) => season.season_number === this.selectedSeason)?.episodes || [];
     },
-  };
+  },
+};
 </script>
 
 <style>
+.active-show-page {
+  font-family: Arial, sans-serif;
+  margin: 20px;
+  max-width: 800px;
+}
+
+.show-title {
+  font-size: 32px;
+}
+
+.show-overview {
+  margin-bottom: 30px;
+}
+
+.seasons-dropdown {
+  margin-bottom: 20px;
+}
+
+.episodes-list {
+  margin-top: 20px;
+}
+
+ul {
+  list-style: none;
+  padding: 0;
+}
+
+li {
+  margin-bottom: 10px;
+}
+
+a {
+  color: #007bff;
+  text-decoration: none;
+}
+
+select {
+  border-radius: 5px;
+  border: 1px solid #ccc;
+  padding: 5px;
+}
 </style>
